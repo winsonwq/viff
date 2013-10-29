2c4eb88b62bc3ea067385f5665edc0af4c73829e
Wang Qiu <winsonwq@gmail.com>
2013-10-29 22:14:49 +0800

    add shell pre-install to replace .coffee

---
+++ scripts/ubuntu-pre-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Installing on Ubuntu"
+echo "Read More in https://github.com/LearnBoost/node-canvas/wiki/Installation---Ubuntu"
+
+sudo apt-get update
+sudo apt-get install libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev build-essential g++
+
+exit 0
